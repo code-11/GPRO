cf634eb68fef572b045c79eede9c8bbe0e517b19
Brendan Ritter <brendan.ritter@yahoo.com>
2014-10-16 23:39:02 -0400

    Started a bit on part 2

--- Level3/Olinland/clock.py
+++ Level3/Olinland/clock.py
@@ -1,7 +1,10 @@
-
+import Queue
 class Clock (object):
 
     def __init__ (self,time):
+        self.todo=Queue.PriorityQueue()
+    def register(f,priority):
+        self.todo.put((priority,f))
+    def unregister():
         pass
-
     # FIX ME
